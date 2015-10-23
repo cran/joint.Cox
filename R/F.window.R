efd7f90a22ec669875c560b591530a869f48bb40
Takeshi Emura <takeshiemura@gmail.com>
2015-10-23 08:50:30 +0000

    version 2.3

--- R/F.window.R
+++ R/F.window.R
@@ -4,6 +4,11 @@ function(time,width,X,Z1,Z2,beta1,beta2,eta,theta,alpha,
 
 if(time<xi1){warning("time should be larger than xi1")}
 if(time+width>=xi3){warning("out-of-prediction bound; time+width should be smaller than xi3")}
+if(X>=xi3){
+  warning("X should be smaller than xi3; forced to be X=xi3")
+  X=xi3
+}
+if(theta<0){warning("in general, theta should be positive or zero")}
 
 ########### M-spline matrix #############
 D=(xi3-xi1)/2
@@ -45,74 +50,84 @@ if(X<xi1){R1_X=0}else{R1_X=as.vector( I_func(X)%*%g1 )}
 if(time<xi1){R2_t=0}else{R2_t=as.vector( I_func(time)%*%g2 )}
 if(time+width<xi1){R2_tw=0}else{R2_tw=as.vector( I_func(time+width)%*%g2 )}
 
-S_t_u=function(u){
-  S1_t=exp( theta*u%*%t( exp(bZ1)*R1_t ) )
-  S2_t=exp( theta*u^alpha%*%t( exp(bZ2)*R2_t ) )
-  (S1_t+S2_t-1)^(-1/theta)
+#### Marginal survival given frailty ####
+S1_t_func=function(u){  exp( -u%*%t( exp(bZ1)*R1_t ) )  }
+S1_X_func=function(u){  exp( -u%*%t( exp(bZ1)*R1_X ) )  }
+S2_t_func=function(u){  exp( -u^alpha%*%t( exp(bZ2)*R2_t ) )  }
+S2_tw_func=function(u){  exp( -u^alpha%*%t( exp(bZ2)*R2_tw ) )  }
+
+### Joint survival #####
+if(theta>0){
+  S_t_func=function(u){
+    A=pmax( S1_t_func(u)^(-theta)+S2_t_func(u)^(-theta)-1,0 )
+    A^(-1/theta)*dgamma(u,shape=1/eta,scale=eta)
+  }
+  S_t=integrate(S_t_func,0.001,10,stop.on.error = FALSE)$value
+
+  S_tw_func=function(u){
+    A=pmax( S1_t_func(u)^(-theta)+S2_tw_func(u)^(-theta)-1,0 )
+    A^(-1/theta)*dgamma(u,shape=1/eta,scale=eta)
+  }
+  S_tw=integrate(S_tw_func,0.001,10,stop.on.error = FALSE)$value
+  
+  S_Xt_func=function(u){
+    A=pmax( S1_X_func(u)^(-theta)+S2_t_func(u)^(-theta)-1,0 )
+    B=u*S1_X_func(u)^(-theta)*A^(-1/theta-1)*dgamma(u,shape=1/eta,scale=eta)
+    pmax(B,0,na.rm=TRUE)
+  }
+  S_Xt=integrate(S_Xt_func,0.001,10,stop.on.error = FALSE)$value
+
+  S_Xtw_func=function(u){
+    A=pmax( S1_X_func(u)^(-theta)+S2_tw_func(u)^(-theta)-1,0 )
+    B=u*S1_X_func(u)^(-theta)*A^(-1/theta-1)*dgamma(u,shape=1/eta,scale=eta)
+    pmax(B,0,na.rm=TRUE)
+  }
+  S_Xtw=integrate(S_Xtw_func,0.001,10,stop.on.error = FALSE)$value
+}else{
+  S_t_func=function(u){
+    S1_t_func(u)*S2_t_func(u)*dgamma(u,shape=1/eta,scale=eta)
+  }
+  S_t=integrate(S_t_func,0.001,10,stop.on.error = FALSE)$value
+  
+  S_tw_func=function(u){
+    S1_t_func(u)*S2_tw_func(u)*dgamma(u,shape=1/eta,scale=eta)
+  }
+  S_tw=integrate(S_tw_func,0.001,10,stop.on.error = FALSE)$value
+  
+  S_Xt_func=function(u){
+    u*S1_X_func(u)*S2_t_func(u)*dgamma(u,shape=1/eta,scale=eta)
+  }
+  S_Xt=integrate(S_Xt_func,0.001,10,stop.on.error = FALSE)$value
+  
+  S_Xtw_func=function(u){
+    u*S1_X_func(u)*S2_tw_func(u)*dgamma(u,shape=1/eta,scale=eta)
+  }
+  S_Xtw=integrate(S_Xtw_func,0.001,10,stop.on.error = FALSE)$value
 }
-
-S_tw_u=function(u){
-  S1_t=exp( theta*u%*%t( exp(bZ1)*R1_t ) )
-  S2_tw=exp( theta*u^alpha%*%t( exp(bZ2)*R2_tw ) )
-  (S1_t+S2_tw-1)^(-1/theta)
-}
-
-func1=function(u){  pmax(S_t_u(u),0,na.rm=TRUE)*dgamma(u,shape=1/eta,scale=eta) }
-S_t=integrate(func1,0.001,10,stop.on.error = FALSE)$value
-
-func2=function(u){  pmax(S_tw_u(u),0,na.rm=TRUE)*dgamma(u,shape=1/eta,scale=eta) }
-S_tw=integrate(func2,0.001,10,stop.on.error = FALSE)$value
-
-func3=function(u){
-  f3=( exp(-u^alpha%*%t(exp(bZ2)*R2_t))-S_t_u(u) )*dgamma(u,shape=1/eta,scale=eta) 
-  pmax(f3,0,na.rm=TRUE)
-}
-S_0t=integrate(func3,0.001,10,stop.on.error = FALSE)$value
-
-func4=function(u){
-  f4=( exp(-u^alpha%*%t(exp(bZ2)*R2_tw))-S_tw_u(u) )*dgamma(u,shape=1/eta,scale=eta) 
-  pmax(f4,0,na.rm=TRUE)
-}
-S_0tw=integrate(func4,0.001,10,stop.on.error = FALSE)$value
-
-func5=function(u){
-  S1_X=exp( theta*u%*%t( exp(bZ1)*R1_X ) )
-  S2_t=exp( theta*u^alpha%*%t( exp(bZ2)*R2_t ) )
-  f5=u*S1_X*(S1_X+S2_t-1)^(-1/theta-1)*dgamma(u,shape=1/eta,scale=eta) 
-  pmax(f5,0,na.rm=TRUE)
-}
-S_Xt=integrate(func5,0.001,10,stop.on.error = FALSE)$value
-
-func6=function(u){
-  S1_X=exp( theta*u%*%t( exp(bZ1)*R1_X ) )
-  S2_tw=exp( theta*u^alpha%*%t( exp(bZ2)*R2_tw ) )
-  f6=u*S1_X*(S1_X+S2_tw-1)^(-1/theta-1)*dgamma(u,shape=1/eta,scale=eta) 
-  pmax(f6,0,na.rm=TRUE)
-}
-S_Xtw=integrate(func6,0.001,10,stop.on.error = FALSE)$value
 
 if((S_tw<=0)|(S_t<=0)){F_noevent=1}else{F_noevent=1-S_tw/S_t}
-if((S_0tw<=0)|(S_0t<=0)){F_event=F_noevent}else{F_event=1-(S_0tw)/(S_0t)}
 if((S_Xtw<=0)|(S_Xt<=0)){F_event_at_X=F_noevent}else{F_event_at_X=1-(S_Xtw)/(S_Xt)}
 
 if(Fplot==TRUE){
   num_grid=500
   x_grid=seq(X,time,length=num_grid)
-  plot(x_grid,rep(0,num_grid),xlim=c(xi1,xi3),ylim=c(0,1),type="l",lwd=4,
-       xlab="t",ylab="Probability of death between t and t+w",col="red")
+  plot(x_grid,rep(0,num_grid),xlim=c(xi1,xi3),ylim=c(-0.05,1.05),type="l",lwd=4,
+       xlab="t",ylab="Probability of death in ( t, t+w )",col="red")
+  polygon(c(time,time+width,time+width,time),c(0,0,1.092,1.092),col=gray(0.95))
   abline(h=0)
   abline(v=time,col="gray")
   abline(v=time+width,col="gray")
-  points(X,0,lwd=4,col="red")
+  points(X,0,lwd=5,col="red")
   p_grid=seq(time,time+width,length=num_grid)
   points(p_grid,rep(F_event_at_X,num_grid),col="red")
   points(p_grid,rep(F_noevent,num_grid),col="blue")
   text(X,0.05,"X",cex=1)
-  text(time,0.05,"t",cex=1)
-  text(time+width,0.05,"t+w",cex=1)
-  text(time,F_event_at_X+0.05,"Event occurred at X (< t)",cex=1)
-  text(time,F_noevent+0.05,"Event not occurred before t",cex=1)
+  text(time,-0.05,"t",cex=1)
+  text(time+width,-0.05,"t+w",cex=1)
+  text(time,F_event_at_X+0.03,"Event at X (< t)",cex=1)
+  text(time,F_noevent+0.03,"No event (X>t)",cex=1)
 }
 
-c(t=time,w=width,X=X,F_event_at_X=F_event_at_X,F_event=F_event,F_noevent=F_noevent)
+c(t=time,w=width,X=X,F_event_at_X=F_event_at_X,F_noevent=F_noevent)
+
 }
